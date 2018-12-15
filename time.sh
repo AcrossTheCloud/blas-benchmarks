fb10d61ac3f1e3175337cd520ac39fe89e5f1a69
Matthew Berryman <matthew@acrossthecloud.net>
2018-12-16 09:27:31 +1100

    5 runs overall

--- time.sh
+++ time.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-time Rscript benchmark-gcbd.R
-time Rscript benchmark-revolution.R
-time Rscript benchmark-sample.R
-time Rscript benchmark-urbanek.R
+for i in {1..5}
+do
+  time Rscript benchmark-gcbd.R
+  time Rscript benchmark-revolution.R
+  time Rscript benchmark-sample.R
+  time Rscript benchmark-urbanek.R
+done
