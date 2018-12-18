dfcaf211fd839c0cabcd2dc861236d3691ae97a8
Matthew Berryman <matthew@acrossthecloud.net>
2018-12-18 12:35:03 +1100

    source mlk vars & run locally compiled copy of R

--- time.sh
+++ time.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
+source /opt/intel/mkl/bin/mklvars.sh intel64
 for i in {1..5}
 do
-  time Rscript benchmark-gcbd.R
-  time Rscript benchmark-revolution.R
-  time Rscript benchmark-sample.R
-  time Rscript benchmark-urbanek.R
+  time ~/R-3.5.1/bin/Rscript benchmark-gcbd.R
+  time ~/R-3.5.1/bin/Rscript benchmark-revolution.R
+  time ~/R-3.5.1/bin/Rscript benchmark-sample.R
+  time ~/R-3.5.1/bin/Rscript benchmark-urbanek.R
 done
