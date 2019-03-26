bb1d583045c2b949e7063bcd6466f390a96ba8af
Matthew Berryman <matthew@acrossthecloud.net>
2019-03-26 13:24:40 +1100

    assume system installed R

--- time.sh
+++ time.sh
@@ -2,8 +2,8 @@
 source /opt/intel/mkl/bin/mklvars.sh intel64
 for i in {1..5}
 do
-  time ~/R-3.5.1/bin/Rscript benchmark-gcbd.R
-  time ~/R-3.5.1/bin/Rscript benchmark-revolution.R
-  time ~/R-3.5.1/bin/Rscript benchmark-sample.R
-  time ~/R-3.5.1/bin/Rscript benchmark-urbanek.R
+  time Rscript benchmark-gcbd.R
+  time Rscript benchmark-revolution.R
+  time Rscript benchmark-sample.R
+  time Rscript benchmark-urbanek.R
 done
